49939cf00a1987c4e5f60716b53e87689b6dc562
André Ghisleni Raimann <andre@andreg.com.br>
2023-03-11 23:54:03 -0300

    Mudanças a serem submetidas: 	new file:   .gitignore 	new file:   README.md 	new file:   node.js 	new file:   package.json 	new file:   react.js 	new file:   yarn.lock

---
+++ node.js
@@ -0,0 +1,109 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'unused-imports',
+    'eslint-plugin-import-helpers'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': [
+          '**/*.spec.ts',
+          '**/*.spec.js',
+          './jest.config.ts',
+          './jest.config.js'
+        ]
+      }
+    ],
+    'no-use-before-define': 'off',
+    'no-nested-ternary': 'off',
+    'import/prefer-default-export': 'off',
+    'camelcase': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'no-useless-constructor': 'off',
+    'import/no-duplicates': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        'argsIgnorePattern': '_'
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never'
+      }
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        'newlinesBetween': 'always',
+        'groups': [
+          'module',
+          '/^@config/',
+          '/^@shared/',
+          '/^@modules/',
+          [
+            'parent',
+            'sibling'
+          ],
+          'index'
+        ],
+        'alphabetize': {
+          'order': 'asc',
+          'ignoreCase': true
+        }
+      }
+    ],
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': [
+          'PascalCase'
+        ],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
+    'no-shadow': 'off',
+    'no-console': 'warn',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off'
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+}
---
+++ react.js
@@ -0,0 +1,134 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'react-hooks',
+    'unused-imports',
+    'eslint-plugin-import-helpers'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+      },
+    ],
+
+
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'no-unused-expressions': 'off',
+    'react/jsx-indent': [
+      2,
+      2,
+      {
+        'checkAttributes': true,
+        'indentLogicalExpressions': true
+      }
+    ],
+    'react/jsx-no-constructed-context-values': 'off',
+    'react/function-component-definition': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'consistent-return': 'off',
+    'class-methods-use-this': 'off',
+    'no-plusplus': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }
+    ],
+    'prettier/prettier': 'error',
+    'no-nested-ternary': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'unused-imports/no-unused-imports-ts': 'error',
+    'import/no-duplicates': 'off',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': [
+          '.tsx'
+        ]
+      }
+    ],
+    'import/prefer-default-export': 'off',
+    'camelcase': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never',
+        'tsx': 'never'
+      }
+    ],
+    'no-use-before-define': 'off',
+    'react/jsx-props-no-spreading': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  }
+}

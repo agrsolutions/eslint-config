6111be1c6d559a9e0b63810381774f483e2c2c30
André Ghisleni Raimann <andre@andreg.com.br>
2024-02-08 20:57:28 -0300

    Changes to be committed: 	new file:   nest.js 	modified:   package.json

---
+++ nest.js
@@ -0,0 +1,145 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: './'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'unused-imports',
+    'eslint-plugin-import-helpers',
+    'import'
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 320,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'avoid',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        'devDependencies': [
+          '**/*.spec.ts',
+          '**/*.spec.js',
+          './jest.config.ts',
+          './jest.config.js'
+        ]
+      }
+    ],
+    'no-use-before-define': 'off',
+    'no-nested-ternary': 'off',
+    'import/prefer-default-export': 'off',
+    'camelcase': 'off',
+    'no-underscore-dangle': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    'no-useless-constructor': 'off',
+    'import/no-duplicates': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        'argsIgnorePattern': '^_',
+        'varsIgnorePattern': '^_',
+        'caughtErrorsIgnorePattern': '^_'
+      }
+    ],
+    'unused-imports/no-unused-imports-ts': 'error',
+    'import/no-duplicates': 'off',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'varsIgnorePattern': '^_',
+        'args': 'after-used',
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'ts': 'never'
+      }
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        'newlinesBetween': 'always',
+        'groups': [
+          'module',
+          '/^@config/',
+          '/^@shared/',
+          '/^@modules/',
+          [
+            'parent',
+            'sibling'
+          ],
+          'index'
+        ],
+        'alphabetize': {
+          'order': 'asc',
+          'ignoreCase': true
+        }
+      }
+    ],
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        'selector': 'interface',
+        'format': [
+          'PascalCase'
+        ],
+        'custom': {
+          'regex': '^I[A-Z]',
+          'match': true
+        }
+      }
+    ],
+    'no-shadow': 'off',
+    'no-console': 'warn',
+    'no-empty-function': 'off',
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    'import/no-cycle': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off'
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      'typescript': {}
+    }
+  },
+}
